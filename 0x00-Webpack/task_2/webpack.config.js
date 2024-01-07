430f91249cffd266dd8a4cf142f63873668e8bef
Matrix <jax64720@gmail.com>
2024-01-07 23:26:27 +0100

    Do not commit package-lock

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js')
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack Output",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+};
