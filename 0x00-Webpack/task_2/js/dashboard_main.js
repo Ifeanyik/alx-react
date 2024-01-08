df784a3d4b92a9df82818186e24e0662c5f5a89f
Matrix <jax64720@gmail.com>
2024-01-08 15:33:04 +0100

    Update gitignore

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,11 +1,13 @@
 import $ from "jquery";
 import _ from "lodash";
+import "../css/main.css"
 
 $(document).ready(() => {
-    let elementArray = ["<p>Holberton Dashboard</p>",
+    let elementArray = ["<img src='../assets/holberton-logo.jpg' id='logo' height='200px' width='200px'>",
+                    "<p>Holberton Dashboard</p>",
                     "<p>Dashboard data for the students</p>",
-                    "<button>Click here to get started</button>",
-                    "<p id='count'></p>",
+                    "<div><button>Click here to get started</button><p id='count'></p></div>",
+                    //"<p id='count'></p>",
                     "<p>Copyright - Holberton School</p>"];
     for (let element of elementArray)
         $("body").append(element);
